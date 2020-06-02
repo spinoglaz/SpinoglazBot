d11947954958b79bd3fae6bf393e008fd7effc65
spinoglaz <spinoglaz@gmail.com>
2020-06-02 13:56:35 +0600

    Remove proxy server settings

--- src/main/java/ru/dasha/spinoglazbot/Main.java
+++ src/main/java/ru/dasha/spinoglazbot/Main.java
@@ -6,9 +6,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
     public static void main(String[] args) {
-        System.getProperties().put( "proxySet", "true" );
-        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-        System.getProperties().put( "socksProxyPort", "9150" );
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
         SpinoglazBot spinoglazBot = new SpinoglazBot();
